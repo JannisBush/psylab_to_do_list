9bddf157d5f083106d094693c31080cacf7a4e1a
ChristophHoenes <christoph.hoenes@gmail.com>
2018-04-25 19:00:47 +0200

    Bugfix (button press did not add any bullet points) and extension to be able to add a bullet point via enter press

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-document.getElementsById("bt").onclick = createBulletPoint(){
-		 					     var text, list;
-		 					     text = tf.value
-						 	     list = document.getElementsByClassName("list")
-		 					     var li = document.createElement("li");
-  							     li.appendChild(document.createTextNode(text));
-							     list.appendChild(li)
-							     }
+function createBulletpoint() {
+ var x = document.getElementById("tf");
+ var text = x.value;
+ var ul = document.getElementById("list");
+ var li = document.createElement("li");
+ li.appendChild(document.createTextNode(text));
+ ul.appendChild(li);
+}
+function createBulletpointEnter(event) {
+ if(event.keyCode == 13) {  
+  var x = document.getElementById("tf");
+  var text = x.value;
+  var ul = document.getElementById("list");
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(text));
+  ul.appendChild(li);
+  return false;
+ }
+}
